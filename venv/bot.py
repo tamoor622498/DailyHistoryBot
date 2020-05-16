abae10833b1daae55f2982f40233b2034ae789aa
Tamoor Hamid <tamoor622498@gmail.com>
2020-05-16 11:08:08 -0400

    Create bot.py
    start

---
+++ venv/bot.py
@@ -0,0 +1,25 @@
+import wikipedia
+import datetime
+import time
+import tweepy
+import random
+import authentication #Holds twitter log in data.
+
+api = authentication.authFunc() #Returns the API object
+
+# Create a tweet
+mydate = datetime.datetime.now()
+s = mydate.strftime("%B")
+s = s + " " + str(mydate.day)
+t = wikipedia.page(s)
+x = t.section("Events")
+f = x.splitlines()
+
+api.update_status("Git Test.")
+
+# for x in range(0,4):
+#     loc = random.randrange(0,len(f))
+#     out = "Today in " + f[loc]
+#     f.pop(loc)
+#     #api.update_status(out)
+#     time.sleep(60)
