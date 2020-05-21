a8736dbf1f658ebee72548442dada3374cee2827
Tamoor Hamid <tamoor622498@gmail.com>
2020-05-21 06:31:55 -0400

    Exceptions

--- venv/bot.py
+++ venv/bot.py
@@ -6,14 +6,13 @@ import random
 
 import authentication  # Holds twitter log in data.
 
-# global variables
 api = authentication.authFunc()  # Returns the API object
 
 
 def getEvents(day):  # Gets the array of events for given day
-    events = wikipedia.page(day)
-    events = events.section("Events")
-    events = events.splitlines()
+    events = wikipedia.page(day)#Wiki page for current day
+    events = events.section("Events")#String of the entire Events section
+    events = events.splitlines()#Turns string in to list of event strings
     return events
 
 
@@ -23,24 +22,32 @@ def daySetUp():
     global currentDay
     global monthAndDay
 
-    mydate = datetime.now()
-    month = mydate.strftime("%B")
-    currentDay = mydate.day
+    mydate = datetime.now()#current date
+    month = mydate.strftime("%B")#String of month name
+    currentDay = mydate.day#Day int
     monthAndDay = month + " " + str(currentDay)
 
 
 def main():
     daySetUp()
-
     events = getEvents(monthAndDay)
-    while (currentDay == mydate.day):
-        now = datetime.now().time()
-        loc = random.randrange(0, len(events))
-        out = monthAndDay + " " + events[loc]
-        events.pop(loc)
-        print(out, "tweeted at ", now)
-        api.update_status(out)
-        time.sleep(3600)
+
+    while (currentDay == mydate.day):#Goes till day changes (RPi reboots at midnight to restart code)
+        if (len(events) < 1):#All string values printed
+            break
+        now = datetime.now().time()#Current time
+        loc = random.randrange(0, len(events))#Random part in list
+        out = monthAndDay + " " + events[loc]#Adds date in front of event
+        events.pop(loc)#Event removes from list
+        try:
+            api.update_status(out)
+            print(out, "tweeted at ", now)
+            time.sleep(3600)
+        except tweepy.error.TweepError as e:
+            if (e.api_code == 187):#If tweet repeated
+                print("Repeated Tweet: ", out)
+            else:
+                print("Exception: ", e.reason)#Anyother reason
 
 
 if __name__ == '__main__':
